1fed873084523a010ea5f01a68c0f95f1712cb8a
Margit Tennosaar <margit.tennosaar@bc.fi>
2021-04-25 20:43:30 +0300

    first init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Business College Helsinki</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <p>Main area </p>
+    </main>
+  );
+};
+
+export default Main;
