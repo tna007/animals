19b2f197320014eb379fc377989d6b98b45fa3bb
tna007 <anh.to@edu.bc.fi>
2021-04-29 10:16:56 +0300

    Add weather data using async axios

--- src/components/Header/Weather.js
+++ src/components/Header/Weather.js
@@ -2,25 +2,40 @@ import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
-const API = "http://api.weatherstack.com/current";
-
 const params = {
   access_key: process.env.REACT_APP_WEATHER_API_KEY,
   query: "Helsinki",
 };
+const URL = `http://api.openweathermap.org/data/2.5/weather?q=${params.query}&units=metric&appid=${params.access_key}`;
 
 const Weather = () => {
   const [weather, setWeather] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  //   useEffect(() => {
-  //     setIsLoading(true);
-  //     axios.get(API, { params }).then((res) => console.log(res.data));
-  //   });
+  useEffect(() => {
+    setIsLoading(true);
+    const getWeather = async () => {
+      const res = await axios.get(URL);
+      const data = res.data;
+      setWeather(data);
+      setIsLoading(false);
+    };
+    getWeather();
+  }, []);
 
+  if (isLoading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
   return (
     <div>
-      <p></p>
+      <p>
+        {weather.name}, {Math.round(weather.main.temp)}°C. Feels like{" "}
+        {Math.round(weather.main.feels_like)}°C
+      </p>
     </div>
   );
 };
