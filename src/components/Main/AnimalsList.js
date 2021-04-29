b532b537b2b4247adbe70fd7b771ddeb8ae71a62
tna007 <anh.to@edu.bc.fi>
2021-04-29 10:17:10 +0300

    Rearrange components

--- src/components/Main/AnimalsList.js
+++ src/components/Main/AnimalsList.js
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import AnimalCard from "./AnimalCard";
-import NewAnimal from "./NewAnimal";
+// import NewAnimal from "./NewAnimal";
 
-import axios from "axios";
-
-function AnimalsList() {
-  const [animals, setAnimals] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/animals")
-      .then((resp) => setAnimals(resp.data));
-  }, []);
-
-  const [newAnimal, setNewAnimal] = useState({
-    name: "",
-    aclass: "",
-    img: "",
-    desc: "",
-    link: "",
-  });
-
-  const changeHandler = (e) => {
-    setNewAnimal({ ...newAnimal, [e.target.name]: e.target.value });
-    console.log(newAnimal);
-  };
-
-  const sendData = (e) => {
-    e.preventDefault();
-    axios.post("http://localhost:3001/animals", newAnimal);
-  };
-
+function AnimalsList({ animals }) {
   return (
     <div className="posts">
       {animals.map((animal) => (
@@ -44,7 +16,6 @@ function AnimalsList() {
           img={animal.img}
         />
       ))}
-      <NewAnimal value={changeHandler} submit={sendData} />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import AnimalsList from "./AnimalsList";
-// import NewAnimal from "./NewAnimal";
-// import axios from "axios";
+import NewAnimal from "./NewAnimal";
+import axios from "axios";
 
 const Main = () => {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/animals")
+      .then((resp) => setAnimals(resp.data));
+  }, []);
+
+  const [newAnimal, setNewAnimal] = useState({
+    name: "",
+    aclass: "mamals",
+    img: "",
+    desc: "",
+    link: "",
+  });
+
+  const changeHandler = (e) => {
+    setNewAnimal({ ...newAnimal, [e.target.name]: e.target.value });
+    console.log(newAnimal);
+  };
+
+  const sendData = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/animals", newAnimal)
+      .then(() => axios.get("http://localhost:3001/animals"))
+      .then((res) => setAnimals(res.data));
+  };
+
   return (
     <main>
-      <AnimalsList />
+      <AnimalsList animals={animals} />
+      <NewAnimal value={changeHandler} submit={sendData} />
     </main>
   );
 };
